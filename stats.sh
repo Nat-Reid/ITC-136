939af243393c3998de9313f1d6ca7756ad7ec60c
Nat-Reid <nathanielreid@gmail.com>
2018-11-19 19:57:46 -0800

    declared variables and edited output
    -used grep to select specific output, and awk to select column of output with wanted info
       -- awk is a full scripting language, but is most commonly used to select columns in output
    -declared as commands as variables then used echo to format and print output nicely

--- stats.sh
+++ stats.sh
@@ -1,5 +1,17 @@
 #!/bin/bash 
-df
-free
-netstat
-w
+
+diskUsed=$( df -h |grep "/dev/sda1" |awk '{print $5}' )
+
+memFree=$( free -mh |grep "Mem:" |awk '{print $7}' )
+
+connections=$( netstat |grep tcp )
+
+currentUsers=$( who |awk '{print $1}' )
+
+echo "This is a snapshot of your current system:
+   Disk Used: $diskUsed
+   Memory Available: $memFree
+   Users Logged In: $currentUsers
+   Open TCP Connections: $connections
+   "
+   
