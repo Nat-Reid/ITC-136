0a583d7ffab05988c38a3862417a75fbd1020fd6
Nat <nathanielreid@gmail.com>
2018-11-19 20:09:08 -0800

    Changes connections output in stats.sh

--- stats.sh
+++ stats.sh
@@ -4,14 +4,17 @@ diskUsed=$( df -h |grep "/dev/sda1" |awk '{print $5}' )
 
 memFree=$( free -mh |grep "Mem:" |awk '{print $7}' )
 
-connections=$( netstat |grep tcp )
+connections=$( netstat |grep tcp |awk '{print $4}' )
 
 currentUsers=$( who |awk '{print $1}' )
 
 echo "This is a snapshot of your current system:
+  
    Disk Used: $diskUsed
    Memory Available: $memFree
    Users Logged In: $currentUsers
-   Open TCP Connections: $connections
+   
+Open TCP Connections: 
+$connections
    "
    
